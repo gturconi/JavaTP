bf62ffc5398487b82d2099666fbca0ae8ffc6d15
gturconi <35380017+gturconi@users.noreply.github.com>
2021-08-13 14:15:44 -0300

    Biblioteca
    Se crearon las entidades del dominio

---
+++ Biblioteca/src/entities/Categoria.java
@@ -0,0 +1,38 @@
+package entities;
+
+public class Categoria {
+
+	
+	private int id;
+	private String descripcion;
+	
+	
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getDescripcion() {
+		return descripcion;
+	}
+	public void setDescription(String descripcion) {
+		this.descripcion = descripcion;
+	}
+	
+	@Override
+	public String toString() {
+		return "Categoria [id=" + id + ", description=" + descripcion + "]";
+	}
+	
+	public Categoria(int id, String description) {
+		this.id = id;
+		this.descripcion = description;
+	}
+	
+	public Categoria() {
+		
+		
+	}
+	
+}
---
+++ Biblioteca/src/entities/Editorial.java
@@ -0,0 +1,36 @@
+package entities;
+
+public class Editorial {
+	
+	private int id;
+	private String nombre;
+	
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getNombre() {
+		return nombre;
+	}
+	public void setName(String nombre) {
+		this.nombre = nombre;
+	}
+	
+	@Override
+	public String toString() {
+		return "Editorial [id=" + id + ", name=" + nombre + "]";
+	}
+	
+	public Editorial(int id, String name) {
+		this.id = id;
+		this.nombre = name;
+	}
+	
+	public Editorial() {
+		
+	}
+	
+
+}
